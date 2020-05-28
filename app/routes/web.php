85cbb293cd53f14c98c5b2c4daf59621124a8e74
mnkhod <eikyuu.hoshi@gmail.com>
2020-05-29 01:03:17 +0800

    Added Category Page V.R

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/categories', function () {
+    return view('categories');
+});
 
 Route::get('/products', function () {
     return view('products',['products' => null]);
